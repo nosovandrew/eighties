e941f2ade50e9bd7835da090c5eb364c9c0d3dcc
Andrew Nosov <andrewnosov@outlook.com>
2021-10-04 23:08:51 +0500

    add simple main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,5 @@
-import { request, gql } from 'graphql-request';
-
 const Home = () => {
-    const query = gql`
-        {
-            getProducts {
-                title
-            }
-        }
-    `;
-    
-    request('http://localhost:3000/api/graphql', query).then((data) =>
-        console.log(data)
-    );
-
-    return (
-        <>
-            <h1>80</h1>
-        </>
-    )
+    return <h1>80</h1>;
 };
 
 export default Home;
