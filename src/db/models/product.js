47d32cc8b6f612c792604427d4ae29c2a5cf7a05
Andrew Nosov <andrewnosov@outlook.com>
2021-10-04 23:02:20 +0500

    add product mongoose model with simple validation

--- src/db/models/product.js
+++ src/db/models/product.js
@@ -1,12 +1,80 @@
 import mongoose from 'mongoose';
 
-const ProductsSchema = new mongoose.Schema({
-    title: {
+// Validation funcs
+const atLeastOneSku = (value) => value.length > 0;
+
+// Schemas
+const SkusSchema = new mongoose.Schema({
+    sku: {
         type: String,
-        required: true,
         trim: true,
+        required: true,
+        index: true,
+        unique: true,
+    },
+    qty: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0,
+    },
+    options: {
+        size: {
+            type: String,
+            enum: ['ONESIZE', 'XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'],
+            required: true,
+            default: 'ONESIZE',
+        },
     },
 });
+
+const ProductsSchema = new mongoose.Schema(
+    {
+        item: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        price: {
+            base: {
+                type: Number,
+                required: true,
+            },
+            currency: {
+                type: String,
+                enum: ['RUB', 'USD'],
+                required: true,
+                default: 'RUB',
+            },
+        },
+        drop: {
+            type: Number,
+            required: true,
+        },
+        features: {
+            type: [
+                {
+                    type: String,
+                    trim: true,
+                },
+            ],
+        },
+        skus: {
+            type: [SkusSchema],
+            validate: [atLeastOneSku, 'At least one SKU required.'],
+        },
+        slug: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+            unique: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports =
     mongoose.models.Product || mongoose.model('Product', ProductsSchema);
