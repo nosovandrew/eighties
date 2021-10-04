24c72cf8967622302e424535ea96d9db3b4c04ff
Andrew Nosov <andrewnosov@outlook.com>
2021-10-04 23:07:37 +0500

    make gql module schema + product type desc

--- src/apollo/elements/product.js
+++ src/apollo/elements/product.js
@@ -2,25 +2,126 @@ import { gql } from 'apollo-server-micro';
 
 import Product from '@/db/models/product';
 
+// graphql schema for product entity
 export const typeDefs = gql`
+    # extend for divide schema on modules (root Query in schema.js)
     extend type Query {
-        getProducts: [Product]!
+        "Get all products in db."
+        products: [Product!]!
+        "Get all products with specific drop number."
+        productsByDrop(drop: Int!): [Product!]!
+        "Get only one product with specific slug string."
+        productBySlug(slug: String!): Product!
+        "Get all drops (releases) in db."
+        drops: [Int!]!
     }
 
+    # add custom types
+    scalar ObjectId
+
+    "Item (product) in shop (related to drop number)."
     type Product {
-        title: String!
+        _id: ObjectId!
+        item: String!
+        price: ProductPrice!
+        features: [String!]!
+        drop: Int!
+        skus: [ProductSkus!]!
+        slug: String!
+    }
+
+    "Used in Product type."
+    type ProductPrice {
+        "Price value"
+        base: Float!
+        currency: PriceCurrency
+    }
+
+    "Used in ProductPrice type."
+    enum PriceCurrency {
+        RUB
+        USD
+    }
+
+    "Used in Product type."
+    type ProductSkus {
+        sku: String!
+        qty: Int!
+        options: SkusOptions!
+    }
+
+    "Used in ProductSkus type."
+    type SkusOptions {
+        size: OptionsSize
+    }
+
+    "Used in Product>SkusOptions type."
+    enum OptionsSize {
+        ONESIZE
+        XXS
+        XS
+        S
+        M
+        L
+        XL
+        XXL
     }
 `;
 
+// graphql resolvers for product entity
 export const resolvers = {
     Query: {
-        getProducts: async () => {
+        // get all products in db
+        products: async () => {
             try {
                 const products = await Product.find({});
-                
+
                 return products;
             } catch (err) {
-                console.log(err);
+                console.error(err);
+            }
+        },
+        // get products with specific drop number
+        productsByDrop: async (_parent, args, _context, _info) => {
+            try {
+                const products = await Product.find({ drop: args.drop });
+
+                return products;
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        /* 
+        // can be useful in future
+        // get product with specific _id (MongoDB ObjectID type) 
+        productById: async (_parent, args, _context, _info) => {
+            try {
+                const product = await Product.findById({ _id: args.id });
+
+                return product;
+            } catch (err) {
+                console.error(err);
+            }
+        }, */
+        // get product with specific slug
+        productBySlug: async (_parent, args, _context, _info) => {
+            // slug scalar is needed!
+            try {
+                const product = await Product.findOne({ slug: args.slug });
+
+                return product;
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        // get all drops in db
+        drops: async () => {
+            try {
+                const drops = await Product.distinct('drop');
+
+                return drops;
+            } catch (err) {
+                console.error(err);
             }
         },
     },
--- src/apollo/schema.js
+++ src/apollo/schema.js
@@ -1,21 +1,37 @@
 import { merge } from 'lodash';
 import { gql } from 'apollo-server-micro';
-import { makeExecutableSchema } from '@graphql-tools/schema'
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+import GraphQLObjectId from '@/apollo/scalars/objectid';
 
 import {
     typeDefs as Product,
     resolvers as productResolvers,
 } from './elements/product';
 
+// empty root Query type for including modules
 const Query = gql`
     type Query {
         _empty: String
     }
 `;
 
-const resolvers = {};
+// empty root Mutation type for including modules
+const Mutation = gql`
+    type Mutation {
+        _empty: String
+    }
+`;
 
+// empty root resolvers obj for including modules
+const resolvers = {
+    // add graphql scalars (custom types)
+    ObjectId: GraphQLObjectId,
+};
+
+// putting everything into a schema
 export const schema = makeExecutableSchema({
-    typeDefs: [Query, Product],
+    typeDefs: [Query, Mutation, Product],
+    // merge glue up all resolvers
     resolvers: merge(resolvers, productResolvers),
 });
