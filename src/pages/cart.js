508db5e246b4e849dec58b9f0bea0c5977dfff07
Andrew Nosov <andrewnosov@outlook.com>
2021-10-08 13:01:41 +0500

    add cart page

---
+++ src/pages/cart.js
@@ -0,0 +1,55 @@
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
+
+import { CartContext } from '@/contexts/cart/context';
+import { increase, decrease, removeFromCart } from '@/contexts/cart/actions';
+
+export default function Cart() {
+    const { state, dispatch } = useContext(CartContext);
+    const { cart } = state;
+    return (
+        <>
+            <h1>Корзина</h1>
+            <ul>
+                {cart.items.length === 0 ? (
+                    <p>Корзина пуста..</p>
+                ) : (
+                    cart.items.map((_item) => {
+                        const { _id, item, qtyForSale } = _item;
+
+                        return (
+                            <li key={_id}>
+                                {`NAME: ${item} QTY: ${qtyForSale}`}{' '}
+                                <button
+                                    onClick={() =>
+                                        dispatch(increase(_id, cart))
+                                    }
+                                >
+                                    +
+                                </button>
+                                {qtyForSale > 1 && (
+                                    <button
+                                        onClick={() =>
+                                            dispatch(decrease(_id, cart))
+                                        }
+                                    >
+                                        -
+                                    </button>
+                                )}
+                                {qtyForSale === 1 && (
+                                    <button
+                                        onClick={() =>
+                                            dispatch(removeFromCart(_id, cart))
+                                        }
+                                    >
+                                        Удалить
+                                    </button>
+                                )}
+                            </li>
+                        );
+                    })
+                )}
+            </ul>
+        </>
+    );
+}
