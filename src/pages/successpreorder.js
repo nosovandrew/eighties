d90d7ca8fec3384445a53f9025a385e07b772022
Andrew Nosov <andrewnosov@outlook.com>
2021-10-20 08:24:15 +0500

    add client-side successpage (need to debug!)

--- src/pages/successpreorder.js
+++ src/pages/successpreorder.js
@@ -1,3 +1,29 @@
-const SuccessPreorder = () => <h1>Заказ создан!</h1>;
+import useSWR from 'swr';
+import { request } from 'graphql-request';
+import { useRouter } from 'next/router';
 
-export default SuccessPreorder;
+import { ORDER_BY_ID } from '@/apollo/client/queries';
+
+export default function SuccessPreorder() {
+    const {
+        query: { id },
+    } = useRouter();
+
+    const fetcher = async (query) => await request(process.env.API_ENDPOINT, query, { OrderId: id });
+
+    const { data, error } = useSWR(
+        ORDER_BY_ID,
+        fetcher
+    )
+
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    console.log(data);
+
+    return (
+        <>
+            <h1>Оформлен</h1>
+            <p>Общая сумма: {data.orderById.total}</p>
+        </>
+    );
+}
