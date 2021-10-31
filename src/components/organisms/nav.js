61d6ba3251e6387a9483eb4025422ada03a2cc85
Andrew Nosov <andrewnosov@outlook.com>
2021-10-31 22:47:46 +0500

    add logo

--- src/components/organisms/nav.js
+++ src/components/organisms/nav.js
@@ -1,14 +1,48 @@
 import { useContext } from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+import Image from 'next/image';
 
+import { StyledTagA } from '@/components/atoms/links';
 import { CartContext } from '@/contexts/cart/context';
 
-export default function Nav () {
+const NavContainer = styled.div`
+    width: calc(100% - 2*var(--basic-spacing));
+    height: 44px;
+    position: fixed;
+    z-index: 10;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const LogoContainer = styled.div`
+    position: absolute;
+    left: calc(50% - 22px);
+`;
+
+export default function Nav() {
     const { state } = useContext(CartContext);
 
     return (
-        <>
-            <Link href='/cart'><a>Cart {state.cart.items.length}</a></Link>
-        </>
+        <NavContainer>
+            <LogoContainer>
+                <Link href='/' passHref>
+                    <StyledTagA>
+                        <Image
+                            alt='Логотип 80 Apparel'
+                            src='/logo.png'
+                            layout='fixed'
+                            width={44}
+                            height={44}
+                        />
+                    </StyledTagA>
+                </Link>
+            </LogoContainer>
+            <Link href='/cart' passHref>
+                <StyledTagA>Cart {state.cart.items.length}</StyledTagA>
+            </Link>
+        </NavContainer>
     );
 }
