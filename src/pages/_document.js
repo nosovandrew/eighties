309d710768f2ec4efa250a4a6313306b3cb1558c
Andrew Nosov <andrewnosov@outlook.com>
2021-11-03 10:26:01 +0500

    link custom font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -46,6 +46,20 @@ export default class MyDocument extends Document {
                         href='https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap'
                         rel='stylesheet'
                     />
+                    {/* pixel font */}
+                    {/* <link
+                        rel='preconnect'
+                        href='https://fonts.googleapis.com'
+                    />
+                    <link
+                        rel='preconnect'
+                        href='https://fonts.gstatic.com'
+                        crossOrigin
+                    />
+                    <link
+                        href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
+                        rel='stylesheet'
+                    /> */}
                 </Head>
                 <body>
                     <Main />
