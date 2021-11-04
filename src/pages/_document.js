d6d3e29f7cbd0820d5e955c90aa6695ca686f312
Andrew Nosov <andrewnosov@outlook.com>
2021-11-04 11:06:20 +0500

    delete duplicates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -48,15 +48,6 @@ export default class MyDocument extends Document {
                     />
                     {/* pixel font */}
                     {/* <link
-                        rel='preconnect'
-                        href='https://fonts.googleapis.com'
-                    />
-                    <link
-                        rel='preconnect'
-                        href='https://fonts.gstatic.com'
-                        crossOrigin
-                    />
-                    <link
                         href='https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
                         rel='stylesheet'
                     /> */}
