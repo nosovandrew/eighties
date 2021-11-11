2a88fbf0e5acbd903eca800166b5faa4d7d1e14f
Andrew Nosov <andrewnosov@outlook.com>
2021-11-11 13:29:19 +0500

    add some styles

--- src/components/atoms/text.js
+++ src/components/atoms/text.js
@@ -11,10 +11,11 @@ export const H1 = styled.h1`
 
 // global h1 at page (big size and weight)
 export const PageH1 = styled.h1`
-    font-size: 1.5em;
+    text-transform: uppercase;
+    font-size: 1.3em;
 
     @media ${media.md} {
-        font-size: 2em;
+        font-size: 1.5em;
     }
 `;
 
@@ -32,4 +33,10 @@ export const TextItem = styled.p`
 // block of text
 export const TextBlock = styled.p`
     text-align: center;
+    line-height: 1.5em;
+    max-width: 80%;
+    
+    @media ${media.md} {
+        max-width: 40%;
+    }
 `;
