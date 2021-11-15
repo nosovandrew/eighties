cb8be97def96b3dd8296c4b558dc564b3fb6d49a
Andrew Nosov <andrewnosov@outlook.com>
2021-11-15 22:48:00 +0500

    add pwa (manifest)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-module.exports = {
-    env: {
-        MONGODB_URI: process.env.MONGODB_URI,
-        API_ENDPOINT: process.env.API_ENDPOINT,
-        CRM_BOT: process.env.CRM_BOT,
-    },
-    reactStrictMode: true,
-};
+// folder types in root dir fixes issues with this modules
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+
+module.exports = withPlugins(
+    [
+        withPWA({
+            pwa: {
+                dest: 'public',
+                disable: process.env.VERCEL_ENV === 'development',
+            },
+        }),
+    ], {
+        env: {
+            MONGODB_URI: process.env.MONGODB_URI,
+            API_ENDPOINT: process.env.API_ENDPOINT,
+            CRM_BOT: process.env.CRM_BOT,
+            VERCEL_ENV: process.env.VERCEL_ENV,
+        },
+        reactStrictMode: true,
+    }
+);
