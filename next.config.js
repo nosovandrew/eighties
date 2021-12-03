e1d4c911de5f47dfe9b7cb0cabe3d177bff8741a
Andrew Nosov <andrewnosov@outlook.com>
2021-12-03 21:25:34 +0500

    FIX pwa manifest issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // folder types in root dir fixes issues with this modules
 const withPlugins = require('next-compose-plugins');
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPlugins(
     [
@@ -8,6 +9,9 @@ module.exports = withPlugins(
             pwa: {
                 dest: 'public',
                 disable: process.env.VERCEL_ENV === 'development',
+                // fix sw issue (may cause other bugs!)
+                runtimeCaching,
+                buildExcludes: [/middleware-manifest.json$/],
             },
         }),
     ], {
